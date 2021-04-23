da1bef8ef1c7d0ffd54f0f889bb9a6e21c189df1
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 19:23:00 -0300

    Atribui a variavel lowerValue o primeiro valor do array

--- fundamentos/bloco_4/dia_2/exercicio1-dia2.js
+++ fundamentos/bloco_4/dia_2/exercicio1-dia2.js
@@ -3,7 +3,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 let arithmeticAverage = 0;
 let highValue = 0;
-let lowerValue = numbers[0];
+let lowerValue = numbers[1];
 let countOdd = 0;
 
 for(let number of numbers){
