f9bafb3eb4a8b20a310ad3cf9fa1cdff8f70913d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 19:53:08 -0300

    Adiciono if/else dentro do for para multiplicar e adicionar os valores no novo array conforme a regra de negocio

--- fundamentos/bloco_4/dia_2/exercicio-bonus-dia2.js
+++ fundamentos/bloco_4/dia_2/exercicio-bonus-dia2.js
@@ -1,13 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let newNumbers = [];
 
-
-  for(let index = 0; index < numbers.length; index += 1){
+for(let index = 0; index < numbers.length; index += 1){
     if(numbers[index+1] != undefined){
         newNumbers.push(numbers[index] * numbers[index + 1]);
     }
     else{
         newNumbers.push(numbers[index] * 2);
     }
-  };
+};
   console.log(newNumbers);
