1da6e12aaca22b735fafbb48ef33555eabe07fc6
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 14:27:11 -0300

    Adiciona exercicio maior valor entre 2 valores

---
+++ fundamentos/bloco_4/maior-valor.js
@@ -0,0 +1,9 @@
+let primeiroValor = 10;
+let segundoValor = 3;
+
+if(primeiroValor > segundoValor){
+    console.log("Maior valor é: " + primeiroValor);
+}
+else{
+    console.log("Maior valor é: " + segundoValor);
+}
