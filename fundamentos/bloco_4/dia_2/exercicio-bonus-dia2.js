0ef7bc359a0657c8d048724866106ade8e354a96
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 19:52:01 -0300

    Atualizo o for para percorrer o array numbers

--- fundamentos/bloco_4/dia_2/exercicio-bonus-dia2.js
+++ fundamentos/bloco_4/dia_2/exercicio-bonus-dia2.js
@@ -1,13 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-      if (numbers[index] > numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
-      };
-    };
+
+  for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index+1] != undefined){
+        newNumbers.push(numbers[index] * numbers[index + 1]);
+    }
+    else{
+        newNumbers.push(numbers[index] * 2);
+    }
   };
-
-  console.log(numbers);
+  console.log(newNumbers);
