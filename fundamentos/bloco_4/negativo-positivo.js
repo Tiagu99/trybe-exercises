fa1e6e4dc8e2936fd3bec60d1f05e21d670b4975
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 14:39:20 -0300

    Adiciona exercicio validar angulo de tringulo

---
+++ fundamentos/bloco_4/negativo-positivo.js
@@ -0,0 +1,11 @@
+let valor = 0;
+
+if(valor > 0){
+    console.log("positive");
+}
+else if(valor < 0){
+    console.log("negative");
+}
+else{
+    console.log("zero");
+}
---
+++ fundamentos/bloco_4/valida-angulo-triangulo.js
@@ -0,0 +1,15 @@
+let primeiroValor = 60;
+let segundoValor = 60;
+let terceiroValor = 60;
+
+if(primeiroValor < 0 || segundoValor < 0 || terceiroValor <0){
+    console.log("Valor de ângulo inválido");
+}
+else{
+    if((primeiroValor + segundoValor + terceiroValor) == 180){
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
+}
