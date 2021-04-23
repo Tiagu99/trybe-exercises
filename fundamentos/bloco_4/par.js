19e40f41c6e5a720ad4a819d03530d67e1ef1c6f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 15:19:35 -0300

    Adiciona exercicio dos pares

---
+++ fundamentos/bloco_4/par.js
@@ -0,0 +1,10 @@
+let primeiroValor = 3;
+let segundoValor = 2;
+let terceiroValor = 5;
+
+if((primeiroValor % 2) == 0 || (segundoValor % 2) == 0 || (terceiroValor % 2) == 0){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
