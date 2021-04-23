8cc46d0925a1f22f2e50173fa01d9c0130d6533d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 19:00:07 -0300

    Adiciona for para resolver o primeiro passo do exercicio

--- fundamentos/bloco_4/dia_2/exercicio1-dia2.js
+++ fundamentos/bloco_4/dia_2/exercicio1-dia2.js
@@ -1,5 +1,5 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let person of names){
-    console.log(person);
+for(let number of numbers){
+    console.log(number);
 };
