fad59e5139dccfcd352abdfe7bc1be721c66bb16
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 15:21:51 -0300

    Adiciona exercicio dos impar

---
+++ fundamentos/bloco_4/impar.js
@@ -0,0 +1,10 @@
+let primeiroValor = 2;
+let segundoValor = 2;
+let terceiroValor = 2;
+
+if((primeiroValor % 2) != 0 || (segundoValor % 2) != 0 || (terceiroValor % 2) != 0){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
