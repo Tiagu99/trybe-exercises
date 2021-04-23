1c448e3c4a36d02d2131cc7abe550c8331884144
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 14:31:21 -0300

    Adiciona exercicio maior valor entre 3 numeros

---
+++ fundamentos/bloco_4/maior-valor-entre-3.js
@@ -0,0 +1,13 @@
+let primeiroValor = 0;
+let segundoValor = 9;
+let terceiroValor = 8;
+
+if(primeiroValor > segundoValor && primeiroValor > terceiroValor){
+    console.log("Maior valor é: " + primeiroValor);
+}
+else if(segundoValor > primeiroValor && segundoValor > terceiroValor){
+    console.log("Maior valor é: " + segundoValor);
+}
+else{
+    console.log("Maior valor é: " + terceiroValor);
+}
