0768a7d0479c4a73b6a25629d512c5e11fca571f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-23 19:11:39 -0300

    Adiciona if dentro do for para solucionar o passo 5

--- fundamentos/bloco_4/dia_2/exercicio1-dia2.js
+++ fundamentos/bloco_4/dia_2/exercicio1-dia2.js
@@ -2,12 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let result = 0;
 let arithmeticAverage = 0;
+let highValue = 0;
 for(let number of numbers){
     result += number;
+    if(highValue < number){
+        highValue = number;
+    };
 };
 
 arithmeticAverage = result / 10;
-
+console.log(`Maior valor do array é: ${highValue}`);
 if(arithmeticAverage > 20){
     console.log("Valor maior que 20");
 }
