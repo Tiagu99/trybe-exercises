322c632a315e0ceedac15215acc7a384e68209de
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 20:15:22 -0300

    Adiciona if/else para criar a regra de preenchimento dos asterisco

--- fundamentos/bloco_4/dia_3/exercicio-5-bonus.js
+++ fundamentos/bloco_4/dia_3/exercicio-5-bonus.js
@@ -4,15 +4,26 @@ let numberOfSpace = Math.floor(n/2);
 
 let numberOfAsterisk = 1;
 
-let asterisk = "";
+let numberOfInsideSpace = 0;
+
+
 
 for(let index = 0; index < Math.floor(n/2); index += 1){
+    let asterisk = "";
+
     for(let indexSpace = 0; indexSpace < numberOfSpace; indexSpace += 1){
-       
+       asterisk += " ";
     };
     for(let indexAsterisk = 0; indexAsterisk < numberOfAsterisk; indexAsterisk += 1){
-       
+       if(indexAsterisk === 0){
+           asterisk += "*";
+       }
+       else if(numberOfAsterisk > 0){
+           asterisk += " ";
+       }
+       else{
+           asterisk += "*";
+       }
     };
     
 };
-}
