3d409a03c0a1414ae906e5d777302680800c69dd
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 18:57:35 -0300

    Adiciona outro for, passa a declaraçao da variavel para dentro do primeiro for

--- fundamentos/bloco_4/dia_3/exercicio-1.js
+++ fundamentos/bloco_4/dia_3/exercicio-1.js
@@ -1,8 +1,9 @@
 let n = 5;
-let asterisk = "";
+
 for (let indexLine = 0; indexLine < n; indexLine += 1){
-    
-    asterisk += "* ";
-    
+    let asterisk = "";
+    for (let indexLine = 0; indexLine < n; indexLine += 1){
+        asterisk += "* ";
+    }
     console.log(asterisk);
 }
