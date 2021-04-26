4cad083958d8a6bdcfee7b5d9e241ce75aac82bd
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 18:55:49 -0300

    Adiciona for e testa output

---
+++ fundamentos/bloco_4/dia_3/exercicio-1.js
@@ -0,0 +1,9 @@
+let n = 5;
+
+for (let indexLine = 0; indexLine < n; indexLine += 1){
+    let asterisk = "";
+    for (let indexLine = 0; indexLine < n; indexLine += 1){
+        asterisk += "* ";
+    }
+    console.log(asterisk);
+}
