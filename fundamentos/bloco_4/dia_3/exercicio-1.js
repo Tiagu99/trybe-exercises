aaa1b647c9eabb75d329f408d30bace0b133de55
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 19:15:38 -0300

    Adiciona um novo for

--- fundamentos/bloco_4/dia_3/exercicio-1.js
+++ fundamentos/bloco_4/dia_3/exercicio-1.js
@@ -2,7 +2,7 @@ let n = 5;
 
 for (let indexLine = 0; indexLine < n; indexLine += 1){
     let asterisk = "";
-    for (let indexLine = 0; indexLine < n; indexLine += 1){
+    for (let indexColumn = 0; indexColumn < n; indexColumn += 1){
         asterisk += "* ";
     }
     console.log(asterisk);
--- fundamentos/bloco_4/dia_3/exercicio-3.js
+++ fundamentos/bloco_4/dia_3/exercicio-3.js
@@ -3,6 +3,10 @@ let asterisk = "";
 let numberOfSpace = n -1;
 
 for(let index = 0; index < n; index += 1){
-    asterisk += "*";
-    console.log(asterisk)
+    for(let indexSpace = 0; indexSpace < n; indexSpace += 1){
+        asterisk += "*";
+    }
+
+    console.log(asterisk);
+    numberOfSpace -= 1;
 }
