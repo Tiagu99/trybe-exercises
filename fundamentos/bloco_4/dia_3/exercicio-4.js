363bdb74ceafadcd82ed9329e2776cefd902f330
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 19:52:29 -0300

    Adiciona dois for e uma variavel para a resolucao

--- fundamentos/bloco_4/dia_3/exercicio-4.js
+++ fundamentos/bloco_4/dia_3/exercicio-4.js
@@ -2,4 +2,15 @@ let n = 5;
 
 let numberOfSpace = Math.floor(n/2);
 
-let numberOfAsterisk = n;
+let numberOfAsterisk = 1;
+
+let asterisk = "";
+
+for(let index = 0; index < n; index += 1){
+    for(let indexSpace = 0; indexSpace < numberOfSpace; indexSpace += 1){
+
+    }
+    for(let indexAsterisk = 0; indexAsterisk < numberOfAsterisk; indexAsterisk += 1){
+
+    }
+}
