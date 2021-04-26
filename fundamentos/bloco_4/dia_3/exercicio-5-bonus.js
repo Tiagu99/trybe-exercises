4481c78f09898ba32d08f5f82f956015458d856d
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 20:10:13 -0300

    Adiciona dois for

--- fundamentos/bloco_4/dia_3/exercicio-5-bonus.js
+++ fundamentos/bloco_4/dia_3/exercicio-5-bonus.js
@@ -5,3 +5,14 @@ let numberOfSpace = Math.floor(n/2);
 let numberOfAsterisk = 1;
 
 let asterisk = "";
+
+for(let index = 0; index < Math.floor(n/2); index += 1){
+    for(let indexSpace = 0; indexSpace < numberOfSpace; indexSpace += 1){
+       
+    };
+    for(let indexAsterisk = 0; indexAsterisk < numberOfAsterisk; indexAsterisk += 1){
+       
+    };
+    
+};
+}
