4569ba51087650a081f0e726163763ea7a9b473a
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-26 19:19:28 -0300

    Adiciona a declaracao da variavel asterisk para dentro do primeiro for

--- fundamentos/bloco_4/dia_3/exercicio-3.js
+++ fundamentos/bloco_4/dia_3/exercicio-3.js
@@ -1,8 +1,9 @@
 let n = 5;
-let asterisk = "";
+
 let numberOfSpace = n -1;
 
 for(let index = 0; index < n; index += 1){
+    let asterisk = "";
     for(let indexSpace = 0; indexSpace < n; indexSpace += 1){
         if(indexSpace < numberOfSpace){
             asterisk += " ";
