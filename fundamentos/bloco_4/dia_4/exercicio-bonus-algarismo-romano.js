530834f9a22ad7a61b928eb764e1bcd579ac8dbc
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-28 13:47:58 -0300

    Adiciona regra de conversao e teste simples

--- fundamentos/bloco_4/dia_4/exercicio-bonus-algarismo-romano.js
+++ fundamentos/bloco_4/dia_4/exercicio-bonus-algarismo-romano.js
@@ -1,8 +1,8 @@
-
-
-
-
 function converteRomano(algarismo){
+  /* A função não trata a ordem nos algarismo.
+     Então sertifique-se que de fato esteja correto
+       o número romano que deseja converter*/
+  
   let numerosRomanos = {
     I: 1,
     V: 5,
@@ -12,7 +12,26 @@ function converteRomano(algarismo){
     D: 500,
     M: 1000
   };
-
+  algarismo = algarismo.toUpperCase();
   let arrayAlgarismo = algarismo.split();
 
-}
+  let resultado = 0;
+  for(let indice = 0;indice < algarismo.length; indice += 1){
+    if(valorRomano(arrayAlgarismo[indice], numerosRomanos) != false){
+      resultado = valorRomano(arrayAlgarismo[indice], numerosRomanos);
+    };
+  };
+  return resultado;
+};
+
+function valorRomano(letra, numerosRomanos){
+  for(let key in numerosRomanos){
+    if(letra == key){
+      return numerosRomanos[key];
+    }
+    else{
+      return false;
+    };
+  };
+};
+converteRomano("X");
