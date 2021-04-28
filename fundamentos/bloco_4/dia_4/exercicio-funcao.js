391615b263405a0adc53e6797c9bc14bed4ae3cf
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-28 11:17:57 -0300

    Resolve exercicio maior numero

--- fundamentos/bloco_4/dia_4/exercicio-funcao.js
+++ fundamentos/bloco_4/dia_4/exercicio-funcao.js
@@ -18,4 +18,17 @@ function verificaPalindromo(palindromo){
   };
 };
 
-console.log(verificaPalindromo("subinoonibus"));
+console.log(verificaPalindromo(subinoonibus));
+
+function maiorValor(numeros){
+  let maiorNum = numeros[0];
+  for(let numero of numeros){
+    if(maiorNum < numero){
+      maiorNum = numero;
+    };
+  };
+  return maiorNum;
+}
+let numerosTest = [2, 4, 10, 9, -1, 4];
+console.log(`O maior valor é: ${maiorValor(numerosTest)}`);
+
