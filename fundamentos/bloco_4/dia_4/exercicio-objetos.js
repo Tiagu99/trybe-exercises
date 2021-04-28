ada0b2b0f1c4028430558f434bf235fe3f705666
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-28 10:22:12 -0300

    Adiciona nova propriedade ao objeto

--- fundamentos/bloco_4/dia_4/exercicio-objetos.js
+++ fundamentos/bloco_4/dia_4/exercicio-objetos.js
@@ -3,5 +3,6 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+info.recorrente = "Sim";
 
-console.log(info.personagem);
+console.log(info);
