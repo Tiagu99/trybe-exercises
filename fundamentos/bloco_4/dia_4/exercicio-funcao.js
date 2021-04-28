f91310f909ebabfcf21cf2b96cf7cc2300931a04
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-28 12:03:27 -0300

    Resolve exercicio maior nome

--- fundamentos/bloco_4/dia_4/exercicio-funcao.js
+++ fundamentos/bloco_4/dia_4/exercicio-funcao.js
@@ -46,3 +46,16 @@ function indiceMaiorValor(numeros){
 
 let arrayTest = [2, 4, 6, 7, 10, 0, -3];
 console.log(`O indice do maior valor é: ${indiceMaiorValor(arrayTest)}`);
+
+function maiorNome (nomes){
+  let tamanho = nomes[0];
+  for(let nome of nomes){
+    if(tamanho.length < nome.length){
+      tamanho = nome;
+    };
+  };
+  return tamanho;
+};
+
+let arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(`O maior nome é: ${maiorNome(arrayNomes)}`);
