c7bac1a3f4c17466d78923003c8c7f5156363927
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-04-28 12:12:11 -0300

    Resolve exercicio repete numero

--- fundamentos/bloco_4/dia_4/exercicio-funcao.js
+++ fundamentos/bloco_4/dia_4/exercicio-funcao.js
@@ -59,3 +59,22 @@ function maiorNome (nomes){
 
 let arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 console.log(`O maior nome é: ${maiorNome(arrayNomes)}`);
+
+function repeteNumero (numeros){
+  let maisRepete = 0;
+  for(let numero of numeros){
+    contador = 0
+    for(let compara of numeros){
+      if(numero === compara){
+        contador += 1;
+      };
+    };
+    if(maisRepete < contador){
+      maisRepete = numero;
+    };
+  };
+  return maisRepete;
+};
+
+let numeroRepetido = [2, 3, 2, 5, 8, 2, 3, 5, 5, 5, 5];
+console.log(`O numero que mais se repete é: ${repeteNumero(numeroRepetido)}`);
