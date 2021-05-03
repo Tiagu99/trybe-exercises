847fe0443c63909189ed75ea19a24ecd3eaf71e1
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-03 15:55:49 -0300

    Adiciona funcao muda cor do quadrado vermelho

--- fundamentos/bloco_5/dia_1/script.js
+++ fundamentos/bloco_5/dia_1/script.js
@@ -18,3 +18,10 @@ function changeText(){
 function changeSideColor(){
   document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
 }
+
+function changeInnerColor(){
+  document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+}
+
+
+
