1deb32d4caf410c8ebc398fa9e3168b10caf7b44
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-04 11:12:55 -0300

    Adiciona div main content

--- fundamentos/bloco_5/dia_2/script.js
+++ fundamentos/bloco_5/dia_2/script.js
@@ -2,3 +2,8 @@ let textH1 = document.createElement('h1');
 textH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 
 document.getElementsByTagName('body')[0].appendChild(textH1);
+
+let mainContent = document.createElement('div');
+mainContent.className = 'main-content';
+
+document.getElementsByTagName('body')[0].appendChild(mainContent);
