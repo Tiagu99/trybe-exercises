64a19765620196fdf8135e1b708f031b7a2ce9b6
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-04 11:20:09 -0300

    Adiciona tag p dentro de center content

--- fundamentos/bloco_5/dia_2/script.js
+++ fundamentos/bloco_5/dia_2/script.js
@@ -1,14 +1,19 @@
 let textH1 = document.createElement('h1');
 textH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 
-document.getElementsByTagName('body')[0].appendChild(textH1);
+document.querySelector('body').appendChild(textH1);
 
 let mainContent = document.createElement('div');
 mainContent.className = 'main-content';
 
-document.getElementsByTagName('body')[0].appendChild(mainContent);
+document.querySelector('body').appendChild(mainContent);
 
 let centerContent = document.createElement('div');
 centerContent.className = 'center-content';
 
 document.querySelector('.main-content').appendChild(centerContent);
+
+let textP = document.createElement('p');
+textP.innerHTML = 'Olá, tudo bem?';
+
+document.querySelector('.center-content').appendChild(textP);
