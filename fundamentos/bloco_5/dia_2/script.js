13ebd1c1c1d20192591464f0d8df36a261f0468a
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-04 11:16:02 -0300

    Adiciona div center content

--- fundamentos/bloco_5/dia_2/script.js
+++ fundamentos/bloco_5/dia_2/script.js
@@ -7,3 +7,8 @@ let mainContent = document.createElement('div');
 mainContent.className = 'main-content';
 
 document.getElementsByTagName('body')[0].appendChild(mainContent);
+
+let centerContent = document.createElement('div');
+centerContent.className = 'center-content';
+
+document.querySelector('.main-content').appendChild(centerContent);
