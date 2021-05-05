834b1268a05152689ede83320531dda2a9ca571c
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-05 11:05:11 -0300

    Adiciona redirecionamento para outra pagina com double click

--- fundamentos/bloco_5/dia_3/main.js
+++ fundamentos/bloco_5/dia_3/main.js
@@ -32,6 +32,11 @@ function updateTextElement(event) {
 //4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 //redirecione para alguma página;
 //4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+  window.open('https://tiagu99.github.io/')
+})
+
+
 
 //5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 //a cor do mesmo;
