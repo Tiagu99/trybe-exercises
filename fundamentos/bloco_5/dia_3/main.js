3e4a588be2355417f704c6e3efa81186b34e4e40
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-05 11:36:56 -0300

    Adiciona evento troca de cor do top3

--- fundamentos/bloco_5/dia_3/main.js
+++ fundamentos/bloco_5/dia_3/main.js
@@ -32,14 +32,24 @@ function updateTextElement(event) {
 //4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 //redirecione para alguma página;
 //4.1. Que tal redirecionar para seu portifólio?
-myWebpage.addEventListener('dblclick', function() {
-  window.open('https://tiagu99.github.io/')
-})
+myWebpage.addEventListener('dblclick', redirectPage)
 
-
+function redirectPage() {
+  window.open('https://tiagu99.github.io');
+}
 
 //5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 //a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', changeColor)
+myWebpage.addEventListener('mouseleave', resetColor)
+
+function changeColor(event) {
+  event.target.style.color = '#2fc18c';
+}
+function resetColor(event) {
+  event.target.style.color = 'white';
+}
 
 //Segue abaixo um exemplo do uso de event.target:
 
