9bd5cea71515e02dbd03d7eaf2c539659eb4ea31
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-20 11:06:12 -0300

    resolve exercicio maior palavra

--- fundamentos/bloco_7/dia_1/exercise2/script2.js
+++ fundamentos/bloco_7/dia_1/exercise2/script2.js
@@ -1,3 +1,17 @@
 const fatorial = (numero) => (numero > 1) ? numero *= fatorial(numero - 1) : numero;
 
 console.log(fatorial(5));
+
+const maiorPalavra = (frase) => {
+  const divideFrase = frase.split(' ');
+  console.log(divideFrase);
+  let maiorPalavra = divideFrase[0];
+  for (let index = 0; index < divideFrase.length; index += 1) {
+    if (maiorPalavra.length < divideFrase[index].length) {
+      maiorPalavra = divideFrase[index];
+    }
+  }
+  return maiorPalavra;
+}
+
+console.log(maiorPalavra('Oi eu Tiago Moreira'));
