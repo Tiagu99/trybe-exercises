d5018724d32fa529505970a4e34b91d3c601273e
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-26 09:52:55 -0300

    resolve exercicio 3

--- fundamentos/bloco_7/dia_3/exercise3-part2.js
+++ fundamentos/bloco_7/dia_3/exercise3-part2.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função sumAllNumbers aqui
-
+const sumAllNumbers = (numbers) => {
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
 
 const numbers = [9, 23, 10, 3, 8];
 const expected = 53;
