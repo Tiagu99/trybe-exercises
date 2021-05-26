e197c98270ddc333b5508f548aa1dc966ddfc5f8
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-25 22:26:03 -0300

    implementa quinto teste

--- fundamentos/bloco_7/dia_3/test4.js
+++ fundamentos/bloco_7/dia_3/test4.js
@@ -13,3 +13,4 @@ assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
 assert.strictEqual(myFizzBuzz(9), 'fizz');
 assert.strictEqual(myFizzBuzz(10), 'buzz');
 assert.strictEqual(myFizzBuzz(8), 8);
+assert.strictEqual(myFizzBuzz('xablau'), false)
