38cedaf8ed70e9b2bd61c70f345f6893f7d2e35f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-25 22:19:21 -0300

    resolver exericcio 3

--- fundamentos/bloco_7/dia_3/test3.js
+++ fundamentos/bloco_7/dia_3/test3.js
@@ -13,3 +13,9 @@ function myRemoveWithoutCopy(arr, item) {
 }
 
 // implemente seus testes aqui
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4, 5], 2), [1, 3, 4, 5]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+const arrayTest = [1, 2, 3, 4, 5];
+myRemoveWithoutCopy(arrayTest, 2);
+assert.deepStrictEqual(arrayTest, [1, 3, 4, 5]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4, 5], 6), [1, 2, 3, 4, 5]);
