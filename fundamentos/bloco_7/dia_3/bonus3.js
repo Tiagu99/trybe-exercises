0bf30e2046c68134919f0e77c6fac6e23aef3101
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-26 12:24:29 -0300

    resolve exercicio bonus 3

--- fundamentos/bloco_7/dia_3/bonus3.js
+++ fundamentos/bloco_7/dia_3/bonus3.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 // escreva a função removeMiddle aqui
-
+const removeMiddle = (words) => {
+  const middle = Math.floor(words.length / 2);
+  return words.splice(middle, 1);
+}
 
 const words = ['mouse', 'giraffe', 'queen', 'window', 'bottle'];
 const expectedWords = ['mouse', 'giraffe', 'window', 'bottle'];
 const expectedOutput = ['queen'];
 const output = removeMiddle(words);
-
 assert.deepStrictEqual(output, expectedOutput);
 assert.deepStrictEqual(words, expectedWords);
