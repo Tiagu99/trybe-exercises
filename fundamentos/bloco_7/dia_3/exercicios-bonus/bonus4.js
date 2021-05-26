31111ed68613a3cc0ec1157ca6a52791f93fa1f9
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-26 12:34:25 -0300

    resolve exercicio bonus 4

--- fundamentos/bloco_7/dia_3/exercicios-bonus/bonus4.js
+++ fundamentos/bloco_7/dia_3/exercicios-bonus/bonus4.js
@@ -1,8 +1,10 @@
+const assert = require('assert');
+
 const getLargestNumber = (array) => {
-  let largestNumber;
-  for (let index = 0; index < array.length - 1; index += 1) {
-      if (array[index] > array[index + 1]) {
-          largestNumber = [array[index]];
+  let largestNumber = 0;
+  for (let index = 0; index < array.length; index += 1) {
+      if (array[index] > largestNumber) {
+          largestNumber = array[index];
       }
   }
   return largestNumber;
@@ -10,3 +12,5 @@ const getLargestNumber = (array) => {
 
 const parameter = [45, 8, 2, 50, 1, 7, 99];
 const result = 99;
+assert.strictEqual(typeof (getLargestNumber(parameter)), 'number');
+assert.strictEqual(getLargestNumber(parameter), result);
