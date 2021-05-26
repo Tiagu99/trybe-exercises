61a6b7d8d659190a40a4242b5815f750da833934
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-25 21:53:56 -0300

    resolve exercicio 1

--- fundamentos/bloco_7/dia_3/test.js
+++ fundamentos/bloco_7/dia_3/test.js
@@ -9,3 +9,8 @@ function sum(a, b) {
 }
 
 // implemente seus testes aqui
+
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => { sum(4, '5')});
+assert.throws(() => { sum(4, '5')}, /^Error: parameters must be numbers$/);
