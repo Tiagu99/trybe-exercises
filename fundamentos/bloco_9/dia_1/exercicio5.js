33e5cbaefb2ce5c9df741852da444fd340a30d96
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-06-21 11:00:18 -0300

    implementa a funcao sendMArsTemperature com callback para resolver o exercicio

--- fundamentos/bloco_9/dia_1/exercicio5.js
+++ fundamentos/bloco_9/dia_1/exercicio5.js
@@ -9,8 +9,12 @@ const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-// definição da função sendMarsTemperature...
-
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => {
+    const temperature = getMarsTemperature();
+    callback(temperature);
+  }, messageDelay());
+}
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
