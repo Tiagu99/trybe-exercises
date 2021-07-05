5b3d80bc762cab4c3b8fe0bbfaaf7b7d42e0f0af
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-05 11:26:52 -0300

    implementa butao para trocar pokemon rederizado na tela

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+        	pokemonPosition: 0
+        }
+				this.changePokemon = this.changePokemon.bind(this);
+    }
+		
+		changePokemon() {
+			if(this.state.pokemonPosition === (pokemons.length - 1)) {
+				this.setState({
+					pokemonPosition: 0
+				});
+			} else {
+				this.setState((previousState, _props) => ({
+						pokemonPosition: previousState.pokemonPosition + 1
+				}));
+			}
+		}
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={pokemons[this.state.pokemonPosition].id} pokemon={pokemons[this.state.pokemonPosition]} />
+								<button onClick={this.changePokemon}>Next Pokemon</button>
             </div>
         );
     }
