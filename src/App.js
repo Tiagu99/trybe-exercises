408a392bf9ab9ac39b76b5a4be010ece3178ddc4
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-17 09:04:48 -0300

    implementa logica para gerar os doginhos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dogImage: '',
+      loading: true,
+    };
+
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  
+
+  fetchDog() {
+    this.setState({
+      loading: true,
+    }, () => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((dog) => this.setState({
+          dogImage: dog.message,
+          loading: false,
+        }));
+    });
+  }
+
+  render() {
+    const { dogImage, loading } = this.state;
+    const load = <h4>loading...</h4>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Dog Images</h1>
+          { loading
+            ? load
+            : <img className="dog-image" src={ dogImage } alt="dog" />}
+          <button type="button" onClick={ this.fetchDog }> Buscar outro</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
